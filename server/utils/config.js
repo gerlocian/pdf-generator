51679d394fd51a1674e349685d94ce8b6793c156
Patrick Ortiz <portiz@signalpath.com>
2017-09-06 16:31:29 -0400

    Added notes for configuration

--- server/utils/config.js
+++ server/utils/config.js
@@ -6,3 +6,6 @@ export const LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'info';
 
 export const NUM_PHANTOMS = process.env.NUM_PHANTOMS || 2;
 export const NUM_NODES = process.env.NUM_NODES || Math.floor(os.cpus().length / (NUM_PHANTOMS + 1));
+
+// log file location
+//
